cdbfb53da86e6005f843e6c02b02feefc5b1814d
mbomhoff <mbomhoff@email.arizona.edu>
2015-08-05 16:08:50 -0700

    Add index option

--- scripts/elasticsearch/copy.pl
+++ scripts/elasticsearch/copy.pl
@@ -5,4 +5,7 @@ use warnings;
 use lib 'perl5';
 use CoGe::Core::Features;
 
-CoGe::Core::Features::copy($ARGV[0]);
+my $dataset_id = shift;
+my $index = shift || 'coge'; # optional index name
+
+CoGe::Core::Features::copy($dataset_id, $index);
